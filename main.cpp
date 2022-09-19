8dd73640629ee31a3ef2ea2827dd12d252a9cae2
SzakacsMark <91319979+SzakacsMark@users.noreply.github.com>
2022-09-19 21:54:16 +0200

    Commit2
    Kijavítottam a hibákat.

--- main.cpp
+++ main.cpp
@@ -4,21 +4,22 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa' // '' helyett "" ,végén hiányzik a ;
-    for (int i = 0;) // nincs befejezve a for ciklus
+    int* b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa";
+    for (int i = 0;i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) // i < N_ELEMENTS
+    std::cout << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:" // hiányzik a b[i] és a ;
-    }    
+        std::cout << "Ertek:" << b[i] << std::endl;
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; // kezdőérték nincs megadva
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
